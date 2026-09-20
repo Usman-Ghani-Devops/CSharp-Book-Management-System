3ab08ab7610e315e773558edee763dc512b61a77
Usman Ghani <usman328ghani@gmail.com>
2026-09-20 10:26:54 +0500

    Added the DataAccess Class and check its working through main class

---
+++ BookDataAccess.cs
@@ -0,0 +1,109 @@
+namespace BookManagementSystem;
+
+class BookDataAccess
+{
+    public void add_book(Book book)
+    {
+        FileStream fs = new FileStream("books.txt", FileMode.Append);
+        StreamWriter writer = new StreamWriter(fs);
+        string line = $"{book.Id}, {book.Title}, {book.Author}, {book.Price}";
+
+        writer.WriteLine(line);
+
+        writer.Close();
+
+        fs.Close();
+    }
+
+    public List<Book> view_all_books()
+
+    {
+        List<Book> books = new List<Book>();
+
+        if (!File.Exists("books.txt"))
+        {
+            return books;
+        }
+
+
+        FileStream fs = new FileStream("books.txt", FileMode.Open);
+
+        StreamReader reader = new StreamReader(fs);
+
+        string? line = reader.ReadLine();
+
+
+        while (line != null)
+        {
+            string[] parts = line.Split(",");
+            Book b1 = new Book();
+            b1.Id = int.Parse(parts[0]);
+            b1.Title = parts[1].Trim();
+            b1.Author = parts[2].Trim();
+            b1.Price = Double.Parse(parts[3].Trim());
+
+            books.Add(b1);
+
+            line = reader.ReadLine();
+        }
+
+        reader.Close();
+        fs.Close();
+        return books;
+    }
+
+    public Dictionary<int, Book> get_book_dictionary()
+    {
+        List<Book> books = view_all_books();
+
+        Dictionary<int, Book> dict = new Dictionary<int, Book>();
+
+        foreach (Book book in books)
+        {
+            if (!dict.ContainsKey(book.Id))
+            {
+                dict.Add(book.Id, book);
+            }
+        }
+
+        return dict;
+    }
+
+    public Book? find_book_by_id(int id)
+    {
+        Dictionary<int, Book> dict = get_book_dictionary();
+
+        if (dict.ContainsKey(id))
+        {
+            return dict[id];
+        }
+
+        return null;
+
+    }
+
+
+    public bool CreateBackup()
+    {
+       if (!File.Exists("books.txt"))
+        {
+            return false;
+        }
+
+        FileStream fs = new FileStream("books.txt", FileMode.Open);
+        FileStream backup = new FileStream("books_backup.txt", FileMode.Create);
+
+        int data = fs.ReadByte();
+
+        while (data != -1)
+        {
+            backup.WriteByte((byte)data);
+            data = fs.ReadByte();
+        }
+
+        fs.Close();
+        backup.Close();
+
+        return true;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,15 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Book book1 = new Book(1,"C#","Welbert",1200);
-        book1.DisplayInfo();
+        Book book1 = new Book(1, "C#", "Welbert", 1200);
+        BookDataAccess book1dal = new BookDataAccess();
 
-        Book book2 = new Book();
-        book2.Id = 2;
-        book2.Title = "C#2";
-        book2.Author = "Welbert02";
-        book2.Price = 1300;
+        book1dal.add_book(book1);
 
-        book2.DisplayInfo();
+        List<Book> books = book1dal.view_all_books();
+
+        if (books.Count == 0)
+        {
+            Console.WriteLine("No books found");
+        }
+
+        foreach (Book book in books)
+        {
+            book.DisplayInfo();
+        }
+
+        Book? bk = book1dal.find_book_by_id(1);
+
+        if (bk == null)
+        {
+            Console.WriteLine("No book Found");
+        }
+        else
+        {
+            bk.DisplayInfo();   
+        }
+
+        bool value = book1dal.CreateBackup();
+        if (value)
+        {
+            Console.WriteLine("Backup Created Successfully");
+        }
+        else
+        {
+            Console.WriteLine("No backup Created");
+        }
+
     }
 }
